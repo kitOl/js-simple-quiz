3e3ce95d5f4d3dd5bbbe406d8447657907b462ed
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-03-06 08:45:31 +0300

    add js

--- main.js
+++ main.js
@@ -1,32 +1,131 @@
 const questions = [
-	{
-		question: "Какой язык работает в браузере?",
-		answers: ["Java", "C", "Python", "JavaScript"],
-		correct: 4,
-	},
-	{
-		question: "Что означает CSS?",
-		answers: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
-		],
-		correct: 2,
-	},
-	{
-		question: "Что означает HTML?",
-		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
-		],
-		correct: 1,
-	},
-	{
-		question: "В каком году был создан JavaScript?",
-		answers: ["1996", "1995", "1994", "все ответы неверные"],
-		correct: 2,
-	},
+  {
+    question: "Какой язык работает в браузере?",
+    answers: ["Java", "C", "Python", "JavaScript"],
+    correct: 4,
+  },
+  {
+    question: "Что означает CSS?",
+    answers: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    correct: 2,
+  },
+  {
+    question: "Что означает HTML?",
+    answers: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    correct: 1,
+  },
+  {
+    question: "В каком году был создан JavaScript?",
+    answers: ["1996", "1995", "1994", "все ответы неверные"],
+    correct: 2,
+  },
 ];
+
+const headerContainer = document.querySelector("#header");
+const listContainer = document.querySelector("#list");
+const submitBtn = document.querySelector("#submit");
+
+let score = 0;
+let questionIndex = 0;
+
+clearPage();
+showQuestion(questionIndex);
+submitBtn.onclick = checkAnswer;
+
+function clearPage() {
+  headerContainer.innerHTML = "";
+  listContainer.innerHTML = "";
+}
+
+function showQuestion() {
+  const headerTemplate = `<h2 class="title">%title%</h2>`;
+  const title = headerTemplate.replace(
+    "%title%",
+    questions[questionIndex]["question"]
+  );
+
+  headerContainer.innerHTML = title;
+
+  let answerNumber = 0;
+  for (answerText of questions[questionIndex]["answers"]) {
+    answerNumber += 1;
+    const questionTemplate = `<li>
+		<label>
+			<input value="${answerNumber}" type="radio" class="answer" name="answer" />
+			<span>%answer%</span>
+		</label>
+	</li>`;
+
+    const answerHTML = questionTemplate.replace("%answer%", answerText);
+    listContainer.innerHTML += answerHTML;
+  }
+}
+
+function checkAnswer() {
+  const checkedRadio = listContainer.querySelector(
+    'input[type="radio"]:checked'
+  );
+
+  if (!checkedRadio) {
+    submitBtn.blur();
+    return;
+  }
+
+  const userAnswer = parseInt(checkedRadio.value);
+
+  if (userAnswer === questions[questionIndex]["correct"]) {
+    score += 1;
+  }
+
+  clearPage();
+  if (questionIndex !== questions.length - 1) {
+    questionIndex += 1;
+    showQuestion();
+    return;
+  } else {
+    showResults();
+  }
+}
+
+function showResults() {
+  console.log("Result score: ", score);
+
+  const resultsTemplate = `<h2 class="title">%title%</h2>
+  <h3 class="summary">%message%</h3>
+  <p class="result">%result%</p>`;
+
+  let title, message;
+  if (score === questions.length) {
+    title = "Поздравляем!";
+    message = "Вы верно ответили на все вопросы!";
+  } else if (score / questions.length >= 0.5) {
+    title = "Неплохой результат!";
+    message = "Вы дали более половины правильных ответов!";
+  } else {
+    title = "Стоит постараться";
+    message = "Пока у вас меньше половины правильных ответов";
+  }
+
+  let result = `${score} из ${questions.length}`;
+
+  const finalMessage = resultsTemplate
+    .replace("%title%", title)
+    .replace("%message%", message)
+    .replace("%result%", result);
+
+  headerContainer.innerHTML = finalMessage;
+
+  submitBtn.blur();
+  submitBtn.innerText = "Начать заново";
+  submitBtn.onclick = () => history.go();
+}
